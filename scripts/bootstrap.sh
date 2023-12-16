b627fc4cc4524fda89e2ddc597fcb8390e15894d
Kody Gaston <43728989+kagaston@users.noreply.github.com>
2023-12-16 01:24:09 -0700

    Update bootstrap.sh: Added additional Comments

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-
+# Install necessary packages
 microdnf -y install java-11-openjdk ncurses procps hostname
-microdnf -y clean all  create_app_user
+# Clean up to reduce the image size
+microdnf -y clean all
+
+# Create a non-root user for Spark
+GUID=spark  # Assuming you want to set GUID to 'spark', add this line
 groupadd --gid 1000 $GUID
 useradd  --uid 1000 \
          --gid $GUID \
          --system \
          --shell /bin/bash \
          --create-home $GUID
-chown $GUID:$GUID $SPARK_HOME
+chown -R $GUID:$GUID $SPARK_HOME  # -R to ensure recursive ownership
 chmod 770 $SPARK_HOME
+
+# Update PATH
 export PATH="${PATH}:${SPARK_HOME}/bin"
 
-
-
-#openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
-#openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
-
+# Uncomment the following lines if you need to generate SSL certificates.
+# They are currently commented out as generating them in a build script might not be the best practice.
+# openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout mykey.key -out mycert.pem
+# openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
